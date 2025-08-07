f4c50a9a2fce57d8f6a932a4b7a351834e1afe6d
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-08 00:02:00 +0530

    Task WebApp Basic

---
+++ Company/src/main/java/com/xworkz/company/dto/BottleDTO.java
@@ -0,0 +1,12 @@
+package com.xworkz.company.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BottleDTO {
+    private String name;
+    private int price;
+}
+
---
+++ Company/src/main/java/com/xworkz/company/dto/SoapDTO.java
@@ -0,0 +1,11 @@
+package com.xworkz.company.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SoapDTO {
+    private String name;
+    private int price;
+}
