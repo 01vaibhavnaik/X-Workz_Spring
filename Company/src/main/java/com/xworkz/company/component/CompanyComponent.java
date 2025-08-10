e2afca5d8089b378b08f763c9273e5fe05642af8
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-10 18:08:41 +0530

    Task WebApp Basic

--- Company/src/main/java/com/xworkz/company/component/CompanyComponent.java
+++ Company/src/main/java/com/xworkz/company/component/CompanyComponent.java
@@ -1,10 +1,11 @@
 package com.xworkz.company.component;
 
-import org.springframework.stereotype.Component;
+import com.xworkz.company.dto.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+@Controller
 @RequestMapping("/")
 public class CompanyComponent {
     public CompanyComponent(){
@@ -12,93 +13,83 @@ public class CompanyComponent {
     }
 
     @RequestMapping("/soap")
-    public String getSoap(@RequestParam String soap,@RequestParam int price){
-        System.out.println("Name :"+soap);
-        System.out.println("Price :"+price);
+    public String getSoap(SoapDTO soapDTO){
+        System.out.println("Name :"+soapDTO.getName()+"Name :"+soapDTO.getPrice());
         System.out.println("Soap is Appling On Body...");
         return "soap.jsp";
     }
     @RequestMapping("/bottle")
-    public String getBottle(@RequestParam String botttleName,@RequestParam int price){
-        System.out.println("Name :"+botttleName);
-        System.out.println("Price :"+price);
+    public String getBottle(BottleDTO bottleDTO){
+        System.out.println("Name :"+bottleDTO.getName()+"Price :"+bottleDTO.getPrice());
         System.out.println("Filling Water In Bottle....");
         return "bottle.jsp";
     }
     @RequestMapping("/butter")
-    public String getButter(@RequestParam String butterName,@RequestParam int price ){
-        System.out.println("Name :"+butterName);
-        System.out.println("Price :"+price);
+    public String getButter(ButterDTO butterDTO){
+        System.out.println("Name :"+butterDTO.getName()+"Price :"+butterDTO.getPrice());
         System.out.println("Appling Butter On Bread....");
         return "butter.jsp";
     }
     @RequestMapping("/biryani")
-    public String getBiryani(@RequestParam String biryaniName,@RequestParam int price){
-        System.out.println("Name :"+biryaniName);
-        System.out.println("Price :"+price);
+    public String getBiryani(BiryanyDTO biryanyDTO){
+        System.out.println("Name :"+biryanyDTO.getName()+"Price :"+biryanyDTO.getPrice());
         System.out.println("Eating Biryani..");
         return "biryani.jsp";
     }
     @RequestMapping("/chappal")
-    public String getChappal(@RequestParam String chappalName,@RequestParam int price ){
-        System.out.println("Name :"+chappalName);
-        System.out.println("Price :"+price);
+    public String getChappal(ChapppalDTO chapppalDTO){
+        System.out.println("Name :"+chapppalDTO.getName()+"Price :"+chapppalDTO.getPrice());
+
         System.out.println("Wearing Chappal..");
         return "chappal.jsp";
     }
     @RequestMapping("/chutney")
-    public String getChutney(@RequestParam String chutney,@RequestParam int price){
-        System.out.println("Name :"+chutney);
-        System.out.println("Price :"+price);
+    public String getChutney(ChutneyDTO chutneyDTO){
+        System.out.println("Name :"+chutneyDTO.getName()+"Price :"+chutneyDTO.getPrice());
         System.out.println("Eating Chutney..");
         return "chutney.jsp";
     }
     @RequestMapping("/curry")
-    public String getCurry(@RequestParam String curry,@RequestParam int price){
-        System.out.println("Name :"+curry);
-        System.out.println("Price :"+price);
+    public String getCurry( CurryDTO curry){
+        System.out.println("Name :"+curry.getName()+"Price :"+curry.getPrice());
         System.out.println("Eating Curry..");
         return "curry.jsp";
     }
     @RequestMapping("/egg")
-    public String getEgg(@RequestParam String egg,@RequestParam int price){
-        System.out.println("Name :"+egg);
-        System.out.println("Price :"+price);
+    public String getEgg(EggDTO egg){
+        System.out.println("Name :"+egg.getName()+"Price :"+egg.getPrice());
         System.out.println("Eating Egg..");
         return "egg.jsp";
     }
     @RequestMapping("/milk")
-    public String getMilk(@RequestParam String milk,@RequestParam int price){
-        System.out.println("Name :"+milk);
-        System.out.println("Price :"+price);
+    public String getMilk(MilkDTO milk){
+        System.out.println("Name :"+milk.getName()+"Price :"+milk.getPrice());
+
         System.out.println("Eating Milk..");
         return "milk.jsp";
     }
     @RequestMapping("/onion")
-    public String getOnion(@RequestParam String onion,@RequestParam int price){
-        System.out.println("Name :"+onion);
-        System.out.println("Price :"+price);
+    public String getOnion(OnionDTO onion){
+        System.out.println("Name :"+onion.getName()+"Price :"+onion.getPrice());
         System.out.println("Eating Onion..");
         return "onion.jsp";
     }
     @RequestMapping("/parotha")
-    public String getParotha(@RequestParam String parotha,@RequestParam int price){
-        System.out.println("Name :"+parotha);
-        System.out.println("Price :"+price);
+    public String getParotha(ParothaDTO parothaDTO){
+        System.out.println("Name :"+parothaDTO.getName()+"Price :"+parothaDTO.getPrice());
+
         System.out.println("Eating parotha..");
         return "parotha.jsp";
     }
     @RequestMapping("/shoe")
-    public String getShoe(@RequestParam String shoe,@RequestParam int price){
-        System.out.println("Name :"+shoe);
-        System.out.println("Price :"+price);
+    public String getShoe(ShoeDTO shoeDTO){
+        System.out.println("Name :"+shoeDTO.getName()+"Price :"+shoeDTO.getPrice());
         System.out.println("Waering Shoe..");
         return "shoe.jsp";
     }
     @RequestMapping("/socks")
-    public String getSocks(@RequestParam String scoks,@RequestParam int price){
-        System.out.println("Name :"+scoks);
-        System.out.println("Price :"+price);
+    public String getSocks(SocksDTO socksDTO){
+        System.out.println("Name :"+socksDTO.getName()+"Price :"+socksDTO.getPrice());
         System.out.println("Waering Socks..");
         return "socks.jsp";
     }
