2ecb772148b5a5ede989807e033dd069444e5172
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-15 14:08:54 +0530

    Form(Getting Result In Index Pg In Spring Side using DTO and Entity Saving In Repo(DataBase)) And Delete Update

--- Form-Spring/src/main/java/com/xworkz/form/repositiory/LoanFormRepositoryImpl.java
+++ Form-Spring/src/main/java/com/xworkz/form/repositiory/LoanFormRepositoryImpl.java
@@ -93,6 +93,123 @@ public class LoanFormRepositoryImpl implements LoanFormRepository {
             }
         return false;
     }
+
+    @Override
+    public boolean updateAddressById(int id,String address) {
+        EntityManagerFactory eMF=null;
+        EntityManager eM = null;
+        EntityTransaction eT = null;
+        try {
+
+            eMF = Persistence.createEntityManagerFactory("loanform");
+            eM = eMF.createEntityManager();
+            eT = eM.getTransaction();
+
+            eT.begin();
+            Query query= eM.createNamedQuery("UpdateAddressByID");
+            query.setParameter("Id",id);
+            query.setParameter("Address",address);
+            query.executeUpdate();
+            eT.commit();
+
+        } catch (Exception e) {
+            if (eT.isActive()) {
+                eT.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            eM.close();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateAAEByName(String name, int age, String address, String email) {
+        EntityManagerFactory eMF=null;
+        EntityManager eM = null;
+        EntityTransaction eT = null;
+        try {
+
+            eMF = Persistence.createEntityManagerFactory("loanform");
+            eM = eMF.createEntityManager();
+            eT = eM.getTransaction();
+
+            eT.begin();
+            Query query=eM.createNamedQuery("UpdateByName");
+            query.setParameter("Name",name);
+            query.setParameter("Age",age);
+            query.setParameter("Address",address);
+            query.setParameter("Email",email);
+            query.executeUpdate();
+            eT.commit();
+
+        } catch (Exception e) {
+            if (eT.isActive()) {
+                eT.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            eM.close();
+        }
+        return false;
+    }
+
+    @Override
+    public List<LoanFormEntity> getAgeBetLoanDetails() {
+        EntityManager eM = null;
+        EntityTransaction eT = null;
+        List<LoanFormEntity> list = null;
+
+        try {
+
+            eMF = Persistence.createEntityManagerFactory("loanform");
+            eM = eMF.createEntityManager();
+            eT = eM.getTransaction();
+
+            eT.begin();
+            Query query= eM.createNamedQuery("GetDetailByAge");
+            list=query.getResultList();
+            eT.commit();
+
+        } catch (Exception e) {
+            if (eT.isActive()) {
+                eT.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            eM.close();
+        }
+        return list;
+    }
+
+    @Override
+    public LoanFormEntity getEmailAddressByName(String name) {
+        EntityManager eM = null;
+        EntityTransaction eT = null;
+        LoanFormEntity loanFormEntity = null;
+
+        try {
+
+            eMF = Persistence.createEntityManagerFactory("loanform");
+            eM = eMF.createEntityManager();
+            eT = eM.getTransaction();
+
+            eT.begin();
+            Query query= eM.createNamedQuery("getEmailAddressByName");
+            query.setParameter("Name",name);
+            loanFormEntity=(LoanFormEntity) query.getSingleResult();
+            eT.commit();
+
+        } catch (Exception e) {
+            if (eT.isActive()) {
+                eT.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            eM.close();
+        }
+        return loanFormEntity;
+    }
 }
 
 
