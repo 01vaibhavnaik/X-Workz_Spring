1dfb6de4e984a3659a71580083b74e489fcc595c
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-17 13:36:49 +0530

    Validation Task

--- Rajajinagar/src/main/java/com/xworkz/rajajinagar/controller/RajajinagarController.java
+++ Rajajinagar/src/main/java/com/xworkz/rajajinagar/controller/RajajinagarController.java
@@ -7,13 +7,13 @@ import com.xworkz.rajajinagar.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("/")
