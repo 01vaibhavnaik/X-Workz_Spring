2dedd0f579f7d2b6b97f1074e631378b09b67242
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-17 13:35:59 +0530

    Validation Task

---
+++ Rajajinagar/src/main/java/com/xworkz/rajajinagar/repository/FlowerRepo.java
@@ -0,0 +1,7 @@
+package com.xworkz.rajajinagar.repository;
+
+import com.xworkz.rajajinagar.entity.FlowerEntity;
+
+public interface FlowerRepo {
+    boolean saveFlower(FlowerEntity flowerEntity);
+}
---
+++ Rajajinagar/src/main/java/com/xworkz/rajajinagar/service/FlowerService.java
@@ -0,0 +1,8 @@
+package com.xworkz.rajajinagar.service;
+
+import com.xworkz.rajajinagar.dto.FlowerDTO;
+
+public interface FlowerService {
+
+    boolean saveFlower(FlowerDTO flowerDTO);
+}
