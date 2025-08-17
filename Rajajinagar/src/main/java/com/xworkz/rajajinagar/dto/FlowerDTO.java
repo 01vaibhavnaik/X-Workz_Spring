aef8bc486967915d5e8eda350be22d7e1d714389
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-17 13:35:06 +0530

    Validation Task

---
+++ Rajajinagar/src/main/java/com/xworkz/rajajinagar/dto/FlowerDTO.java
@@ -0,0 +1,19 @@
+package com.xworkz.rajajinagar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@NonNull
+public class FlowerDTO {
+    @Size(min = 4,max = 15,message = "Letter Should be minimum 4 and max 15 ")
+    private String flowername;
+    private int flowerprice;
+    private String flowercolor;
+}
