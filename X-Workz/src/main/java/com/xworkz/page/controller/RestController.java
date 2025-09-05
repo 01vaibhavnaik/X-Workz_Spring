e416331abbe1321cdef2a4ce1d8a248a76beecd0
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-09-05 09:33:14 +0530

    "Xworkz"

--- X-Workz/src/main/java/com/xworkz/page/controller/RestController.java
+++ X-Workz/src/main/java/com/xworkz/page/controller/RestController.java
@@ -1,29 +1,33 @@
 package com.xworkz.page.controller;
 
 import com.xworkz.page.service.SignUpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/")
+@Slf4j
 public class RestController {
 
     @Autowired
     SignUpService signUpService;
 
-    @GetMapping("checkEmail/{email}")
-    private String checkCount(@PathVariable String email){
-        int count=signUpService.getCount(email);
-        if (count==0)
+    @GetMapping(value = "checkEmail/{emailName}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String checkCount(@PathVariable String emailName){
+        log.info(emailName);
+        int count=signUpService.getCount(emailName);
+        if (count==0){
             return "Email Valid";
-        else  return "Email already Exist";
-
+        }
+        else  {
+            return "Email already Exist";
+        }
 
     }
-
-
 
 
 }
