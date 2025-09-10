0d8114d265a8c58420fcf0615c6f0b17b533e1ff
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-09-10 13:14:19 +0530

    "Xworkz"

--- X-Workz/src/main/java/com/xworkz/page/controller/XworkzController.java
+++ X-Workz/src/main/java/com/xworkz/page/controller/XworkzController.java
@@ -5,11 +5,19 @@ import com.xworkz.page.service.SignUpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 @Controller
 @RequestMapping("/")
@@ -21,20 +29,25 @@ public class XworkzController {
     SignUpService signUpService;
 
     @RequestMapping("/signup")
-    public ModelAndView getdetail(@Valid SignUpDTO signUpDTO, BindingResult bindingResult, ModelAndView modelAndView) {
+    public ModelAndView getdetail(@RequestParam("image") MultipartFile multipartFile,@Valid SignUpDTO signUpDTO, BindingResult bindingResult, ModelAndView modelAndView) throws IOException {
         if (bindingResult.hasErrors()) {
             modelAndView.addObject("errors", bindingResult.getAllErrors());
             modelAndView.setViewName("SignIn");
             return modelAndView;
         }
+
+        byte[] bytes=multipartFile.getBytes();
+        Path path= Paths.get("D:\\ImageData\\"+signUpDTO.getName()+System.currentTimeMillis()+".jpg");
+        Files.write(path,bytes);
+        String imageName=path.getFileName().toString();
         signUpService.getSignUp(signUpDTO);
         modelAndView.setViewName("SignIn");
         return modelAndView;
     }
 
 
-    @RequestMapping("/signin")
-    public ModelAndView getdetail(@Valid String email, String password, ModelAndView modelAndView, HttpSession session) {
+    @PostMapping("/signin")
+    public ModelAndView getdetail( @Valid String email, String password, ModelAndView modelAndView, HttpSession session) {
         SignUpDTO signUpDTO;
         signUpDTO = signUpService.getSignin(email, password);
         if (signUpDTO.getEmail() == null) {
