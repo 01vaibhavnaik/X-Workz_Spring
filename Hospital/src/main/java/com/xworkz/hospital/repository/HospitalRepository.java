b0423a5c205f9f9bb8d33c0b8ba62d2e60223d21
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-09-11 22:13:51 +0530

    HospitalWebApp

--- Hospital/src/main/java/com/xworkz/hospital/repository/HospitalRepository.java
+++ Hospital/src/main/java/com/xworkz/hospital/repository/HospitalRepository.java
@@ -1,7 +1,12 @@
 package com.xworkz.hospital.repository;
 
+import com.xworkz.hospital.entity.DoctorEntity;
 import com.xworkz.hospital.entity.HospitalEntity;
+
+import java.util.List;
 
 public interface HospitalRepository {
     long getDetail(String email);
+    void saveDetails(DoctorEntity doctorEntity);
+    List<DoctorEntity> viewDetail();
 }
