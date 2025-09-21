41ed8e17520b7636bb0af5c9f1a4b2dd9237cb28
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-09-21 19:10:52 +0530

    HospitalWebApp

--- Hospital/src/main/java/com/xworkz/hospital/service/HospitalService.java
+++ Hospital/src/main/java/com/xworkz/hospital/service/HospitalService.java
@@ -1,6 +1,7 @@
 package com.xworkz.hospital.service;
 
 import com.xworkz.hospital.dto.DoctorDTO;
+import com.xworkz.hospital.dto.TimeSlotDTO;
 import com.xworkz.hospital.entity.HospitalEntity;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface HospitalService {
     void saveDetails(DoctorDTO doctorDTO);
     List<DoctorDTO> viewDetail();
     boolean logIn(String otp,String email);
+    void saveSlot(TimeSlotDTO slotDTO);
 }
